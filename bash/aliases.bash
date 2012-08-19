037a936ccf7eb7d1b59c74be35ca0aab029c5290
Matt Gollob <mattgollob@gmail.com>
2012-08-19 09:54:18 -0400

    Add aliases for ls, df, du, diff, and patch

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,6 +1,23 @@
 alias reload='source $HOME/.bashrc'
 alias ls='ls --color=auto'
-alias ll='ls -al'
+alias ll='ls -lh'
+alias lla='ls -alh'
+alias la='ls -a'
 alias grep='grep --color=auto'
 alias more='less'
-which colordiff > /dev/null 2>&1 && alias diff='colordiff -u'
+alias df='df -h'
+alias du='du -h'
+alias ndiff='`which diff` --normal'
+alias diff='diff -u'
+alias cdiff='colordiff -u'
+alias npatch='`which patch` --normal'
+alias patch='patch -u'
+
+pidlist() {
+  ps -eo pid,cmd | grep "$1" | grep -v grep | awk '{ print $1 }' | tr '\n' ',' | sed 's/.$//'
+}
+
+mkcd() {
+  mkdir -p "$*"
+  cd "$*"
+}

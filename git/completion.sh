b579c221d61a5972e0f2c3083e89b3cdd87eb27a
Matt Gollob <mattgollob@gmail.com>
2012-08-30 08:32:03 -0400

    Update git completion to source git-prompt.sh too

--- git/completion.sh
+++ git/completion.sh
@@ -1,3 +1,8 @@
 completion=/usr/share/git/completion/git-completion.bash
+gitprompt=/usr/share/git/completion/git-prompt.sh
 
 [[ -f $completion ]] && . $completion
+[[ -f $gitprompt ]] && . $gitprompt
+
+unset completion
+unset gitprompt

7d31af12376f90d83024765281ddb11567be3494
Matt Gollob <mattness@users.noreply.github.com>
2025-03-04 01:01:27 -0500

    Make install.sh platform-neutral and (almost) no-prereq

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 targetdir=${1:-${HOME}/.dotfiles}
+repourl=https://github.com/mattness/dotfiles
 case `uname` in
-  Linux)  branch=master ;;
-  Darwin) branch=osx ;;
+  Linux)  branch=linux ;;
+  Darwin) branch=osx-ng ;;
   *)       >&2 echo Unknown OS! Aborting...; exit 1 ;;
 esac
 
-NONINTERACTIVE=1 /bin/bash -c "`curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh`" \
-  && hash -r                                                                                                    \
-  && git clone --branch "${branch}" --recursive https://github.com/mattness/dotfiles.git "${targetdir}"         \
-  && eval "`/opt/homebrew/bin/brew shellenv`"                                                                   \
-  && /opt/homebrew/bin/brew bundle install --no-lock --file <(curl -fsSL https://dotfiles.gollob.net/Brewfile)  \
-  && "${targetdir}/install" <<<'B'
+# TODO(mgollob): On linux, use /etc/os-release to determine server vs desktop dotfiles?
+
+if command -v git >/dev/null 2>&1; then
+  git clone --recurse-submodules --branch "${branch}" -- "${repourl}.git" "${targetdir}"
+  "${targetdir}/bootstrap"
+else
+  mkdir -p "${targetdir}"
+  curl -fsSL "${repourl}/archive/refs/heads/${branch}.tar.gz" | tar zxf - -C "${targetdir}" --strip-components 1
+  "${targetdir}/bootstrap"
+  git -c init.defaultBranch="${branch}" init "${targetdir}"
+  git -C "${targetdir}" remote add -f -m "${branch}" origin "${repourl}.git"
+  git -C "${targetdir}" reset "origin/${branch}"
+  git -C "${targetdir}" submodule update --init
+fi

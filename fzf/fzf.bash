44b5d640c7b8d4595ad226e80c5ea39bf6cc2c4e
Matt Gollob <mattness@users.noreply.github.com>
2025-09-15 16:24:04 -0400

    [fzf] Support versions prior to 0.48.0 (ubuntu)

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,3 +1,7 @@
 if command -v fzf &>/dev/null; then
-  eval "$(fzf --bash)"
+  if fzf --bash &>/dev/null; then
+    eval "$(fzf --bash)"
+  elif [ -r '/usr/share/doc/fzf/examples/key-bindings.bash' ]; then
+    source '/usr/share/doc/fzf/examples/key-bindings.bash'
+  fi
 fi

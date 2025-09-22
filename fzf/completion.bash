cab6e39c77a01b5ebfffdf37769317b66a1c28b0
Matt Gollob <mattness@users.noreply.github.com>
2025-09-22 13:37:57 -0400

    [fzf] Separate completion-related items from config

---
+++ fzf/completion.bash
@@ -0,0 +1,19 @@
+if command -v fzf &>/dev/null; then
+  if ! complete | grep -q '_fzf'; then
+    _completion_loader fzf &>/dev/null
+  fi
+fi
+
+# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+if command -v fd &>/dev/null; then
+  _fzf_compgen_path() {
+    fd --hidden --follow --exclude ".git" . "$1"
+  }
+
+  # Use fd to generate the list for directory completion
+  _fzf_compgen_dir() {
+    fd --type d --hidden --follow --exclude ".git" . "$1"
+  }
+fi

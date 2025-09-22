ace490707c3f9e59975a132c18cec16ad2e09a5e
Matt Gollob <mattness@users.noreply.github.com>
2025-09-22 11:47:05 -0400

    [system] Make sure ~/.local/bin is in PATH

--- system/path.bash
+++ system/path.bash
@@ -1,1 +1,12 @@
-export PATH="${dotfilesdir}/bin:${PATH}"
+path_dirs=(
+  "${dotfilesdir}/bin"
+  "${HOME}/.local/bin"
+)
+
+for d in "${path_dirs[@]}"; do
+  if ! _dir_is_in_path "${d}"; then
+    export PATH="${d}:${PATH}"
+  fi
+done
+
+unset path_dirs
